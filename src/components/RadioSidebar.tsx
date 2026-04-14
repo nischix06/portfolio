b02b92580193f1c6f0f24713d92b1de09aa4be6e
nichix06 <arnavsood134@gmail.com>
2026-04-14 19:36:06 +0530

    feat: add RadioSidebar component with streaming audio and Spotify playlist integration

--- src/components/RadioSidebar.tsx
+++ src/components/RadioSidebar.tsx
@@ -147,7 +147,7 @@ export default function RadioSidebar() {
                 <div className="rs-pl-name">{pl.icon} {pl.name}</div>
                 <iframe
                   src={`https://open.spotify.com/embed/playlist/${pl.id}?utm_source=generator&theme=0`}
-                  width="100%" height="80" frameBorder="0"
+                  width="100%" height="152" frameBorder="0"
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                   loading="lazy"
                   style={{ borderRadius: "8px", border: "none" }}
